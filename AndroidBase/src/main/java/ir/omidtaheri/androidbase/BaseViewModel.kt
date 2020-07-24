19b78e19b385a5eed982c453fd1d72f1766ad49e
OmidTaheri <m.omidtaheri@yahoo.com>
2020-07-24 11:36:25 +0430

    improve BaseViewModel and BaseFragment

--- AndroidBase/src/main/java/ir/omidtaheri/androidbase/BaseViewModel.kt
+++ AndroidBase/src/main/java/ir/omidtaheri/androidbase/BaseViewModel.kt
@@ -1,44 +1,58 @@
 package ir.omidtaheri.androidbase
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import ir.omidtaheri.androidbase.singleLiveData.SingleLiveData
 
-class BaseViewModel : ViewModel() {
+open class BaseViewModel<DataLiveType>(application: Application) : AndroidViewModel(application) {
+    val ApplicationClass: Application
 
+    init {
+        ApplicationClass = application
+    }
 
-    private val _isLoading: MutableLiveData<Boolean>
-    val isLoading
+    protected val _isLoading: MutableLiveData<Boolean>
+    val isLoading: LiveData<Boolean>
         get() = _isLoading
 
 
-    private val _ErrorString: MutableLiveData<String>
-    val ErrorString
-        get() = _ErrorString
+    protected val _ErrorSnackBar: SingleLiveData<String>
+    val ErrorSnackBar: LiveData<String>
+        get() = _ErrorSnackBar
 
 
-    private val _ErrorResId: MutableLiveData<Int>
-    val ErrorResId
-        get() = _ErrorResId
+    protected val _ErrorToast: SingleLiveData<String>
+    val ErrorToast: LiveData<String>
+        get() = _ErrorToast
 
 
-    private val _MessageString: MutableLiveData<String>
-    val MessageString
-        get() = _MessageString
+    protected val _MessageSnackBar: SingleLiveData<String>
+    val MessageSnackBar: LiveData<String>
+        get() = _MessageSnackBar
 
 
-    private val _MessageResId: MutableLiveData<Int>
-    val MessageResId
-        get() = _MessageResId
+    protected val _MessageToast: SingleLiveData<String>
+    val MessageToast: LiveData<String>
+        get() = _MessageToast
+
+
+    protected val _DataLive: MutableLiveData<DataLiveType>
+    val DataLive: LiveData<DataLiveType>
+        get() = _DataLive
 
 
     init {
         _isLoading = MutableLiveData()
-        _ErrorString = MutableLiveData()
-        _ErrorResId = MutableLiveData()
-        _MessageString = MutableLiveData()
-        _MessageResId = MutableLiveData()
+        _ErrorSnackBar = SingleLiveData()
+        _ErrorToast = SingleLiveData()
+        _MessageSnackBar = SingleLiveData()
+        _MessageToast = SingleLiveData()
+        _DataLive = MutableLiveData()
     }
 
 
---
+++ AndroidBase/src/main/java/ir/omidtaheri/androidbase/singleLiveData/SingleLiveData.kt
@@ -0,0 +1,49 @@
+package ir.omidtaheri.androidbase.singleLiveData
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveData<T> : MutableLiveData<T>() {
+
+    companion object {
+        private const val TAG = "SingleLiveEvent"
+    }
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        // Observe the internal MutableLiveData
+        super.observe(owner, Observer<T> { t ->
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        })
+    }
+
+    private val mPending = AtomicBoolean(false)
+
+
+    @MainThread
+    override fun setValue(t: T?) {
+        mPending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        this.value = null
+    }
+
+}
