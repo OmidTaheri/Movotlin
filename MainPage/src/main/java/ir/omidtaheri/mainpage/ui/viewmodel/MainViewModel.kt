c8c41004f83fdf1a7a22d94b6043b167bbce8074
OmidTaheri <m.omidtaheri@yahoo.com>
2020-07-24 11:39:04 +0430

    update fragment and viewmodel by base classes

--- MainPage/src/main/java/ir/omidtaheri/mainpage/ui/viewmodel/MainViewModel.kt
+++ MainPage/src/main/java/ir/omidtaheri/mainpage/ui/viewmodel/MainViewModel.kt
@@ -1,10 +1,96 @@
 package ir.omidtaheri.mainpage.ui.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import ir.omidtaheri.androidbase.BaseViewModel
+import ir.omidtaheri.domain.datastate.DataState
+import ir.omidtaheri.domain.datastate.MessageHolder
+import ir.omidtaheri.domain.datastate.UiComponentType
+import ir.omidtaheri.domain.entity.MovieDomainEntity
 import ir.omidtaheri.domain.interactor.GetMovies
+import ir.omidtaheri.mainpage.entity.MovieUiEntity
 import ir.omidtaheri.mainpage.mapper.MovieEntityUiDomainMapper
 
-class MainViewModel (GetMoviesUseCase: GetMovies,UiDomainMapper : MovieEntityUiDomainMapper): BaseViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(
+    val GetMoviesUseCase: GetMovies,
+    val UiDomainMapper: MovieEntityUiDomainMapper,
+    application: Application
+) :
+    BaseViewModel<List<MovieUiEntity>>(application) {
+
+
+    fun getMovieList() {
+        _isLoading.value = true
+        val disposable = GetMoviesUseCase.execute(Unit).subscribeBy { response ->
+            when (response) {
+
+                is DataState.SUCCESS -> {
+                    _isLoading.value = false
+                    _DataLive.value = response.data?.map {
+                        UiDomainMapper.mapToUiEntity(it)
+                    }
+                }
+
+
+                is DataState.ERROR -> {
+                    _isLoading.value = false
+                    response.let { errorDataState ->
+
+                        when (errorDataState.stateMessage?.uiComponentType) {
+                            is UiComponentType.SNACKBAR -> {
+                                HandleSnackBarError(errorDataState)
+                            }
+
+                            is UiComponentType.TOAST -> {
+                                HandleToastError(errorDataState)
+                            }
+                        }
+
+
+                    }
+
+
+                }
+
+            }
+        }
+
+        addDisposable(disposable)
+    }
+
+    private fun HandleSnackBarError(errorDataState: DataState.ERROR<List<MovieDomainEntity>>) {
+        errorDataState.stateMessage!!.message.let { messageHolder ->
+
+            when (messageHolder) {
+                is MessageHolder.MESSAGE -> _ErrorSnackBar.value =
+                    messageHolder.Message
+                is MessageHolder.Res -> _ErrorSnackBar.value =
+                    ApplicationClass.getString(
+                        messageHolder.ResId
+                    )
+
+
+            }
+
+        }
+    }
+
+    private fun HandleToastError(errorDataState: DataState.ERROR<List<MovieDomainEntity>>) {
+        errorDataState.stateMessage!!.message.let { messageHolder ->
+
+            when (messageHolder) {
+                is MessageHolder.MESSAGE -> _ErrorToast.value =
+                    messageHolder.Message
+                is MessageHolder.Res -> _ErrorToast.value =
+                    ApplicationClass.getString(
+                        messageHolder.ResId
+                    )
+
+
+            }
+
+        }
+    }
 }
--- MainPage/src/main/java/ir/omidtaheri/mainpage/ui/viewmodel/MainViewModelFactory.kt
+++ MainPage/src/main/java/ir/omidtaheri/mainpage/ui/viewmodel/MainViewModelFactory.kt
@@ -1,5 +1,6 @@
 package ir.omidtaheri.mainpage.ui.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import ir.omidtaheri.domain.interactor.GetMovies
@@ -8,9 +9,10 @@ import javax.inject.Inject
 
 class MainViewModelFactory @Inject constructor(
     val GetMoviesUseCase: GetMovies,
-    val UiDomainMapper: MovieEntityUiDomainMapper
+    val UiDomainMapper: MovieEntityUiDomainMapper,
+    val application: Application
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return MainViewModel(GetMoviesUseCase, UiDomainMapper) as T
+        return MainViewModel(GetMoviesUseCase, UiDomainMapper,application) as T
     }
 }
