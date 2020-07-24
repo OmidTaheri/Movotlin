eb01c3ba1353d05f55ef0464c0ed0f60f4dc3541
OmidTaheri <m.omidtaheri@yahoo.com>
2020-07-24 11:38:12 +0430

    update DataState

--- Domain/src/main/java/ir/omidtaheri/domain/datastate/DataState.kt
+++ Domain/src/main/java/ir/omidtaheri/domain/datastate/DataState.kt
@@ -2,7 +2,7 @@ package ir.omidtaheri.domain.datastate
 
 sealed class DataState<T> {
 
-    class SUCCESS<T>(data:T? , stateMessage:StateMessage?=null):DataState<T>()
+    class SUCCESS<T>(val data:T? , val stateMessage:StateMessage?=null):DataState<T>()
 
-    class ERROR<T>(stateMessage:StateMessage?=null): DataState<T>()
+    class ERROR<T>(val stateMessage:StateMessage?=null): DataState<T>()
 }
