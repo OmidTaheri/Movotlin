c6cb4f34defa1bd123de7add90042a10884912ca
OmidTaheri <m.omidtaheri@yahoo.com>
2020-07-24 11:36:25 +0430

    improve BaseViewModel and BaseFragment

--- AndroidBase/src/main/java/ir/omidtaheri/androidbase/BaseFragment.kt
+++ AndroidBase/src/main/java/ir/omidtaheri/androidbase/BaseFragment.kt
@@ -6,50 +6,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
 
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment<DataLiveType> : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    protected lateinit var viewModel: BaseViewModel<DataLiveType>
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return InflateViewBinding(inflater, container)
     }
 
-    override fun onAttach(activity: Activity) {
-        super.onAttach(activity)
-    }
+
+    abstract fun InflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): View?
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bindUiComponent()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
+    abstract fun bindUiComponent()
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        ConfigDaggerComponent()
+        SetViewModel()
+        SetLivaDataObserver()
     }
 
-    override fun onDetach() {
-        super.onDetach()
+    private fun SetLivaDataObserver() {
+        setLoadingLiveDataObserver()
+        setToastErrorLiveDataObserver()
+        setSnackBarErrorLivaDataObserver()
+        setToastMessageLiveDataObserver()
+        setSnackBarMessageLiveDataObserver()
+        setDataLiveObserver()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
+    abstract fun setDataLiveObserver()
 
-    override fun onDestroy() {
-        super.onDestroy()
-    }
+    abstract fun setSnackBarMessageLiveDataObserver()
 
-    abstract fun setUp()
+    abstract fun setToastMessageLiveDataObserver()
 
-    abstract fun showLoading()
-    abstract fun hideLoading()
+    abstract fun setSnackBarErrorLivaDataObserver()
 
-    abstract fun showStringError(message: String)
-    abstract fun showResError(ResId: Int)
+    abstract fun setToastErrorLiveDataObserver()
 
+    abstract fun setLoadingLiveDataObserver()
+
+
+    abstract fun ConfigDaggerComponent()
+
+    abstract fun SetViewModel()
+
+    abstract fun showLoading(show: Boolean)
 
     abstract fun showSnackBar(message: String)
     abstract fun showToast(message: String)
-
-
     abstract fun showDialog(message: String)
 }
