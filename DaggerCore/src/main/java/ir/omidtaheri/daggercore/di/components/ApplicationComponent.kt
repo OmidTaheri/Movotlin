221630297f177ef7b30d0efe5bba030db923c1c1
OmidTaheri <m.omidtaheri@yahoo.com>
2020-08-04 15:33:49 +0430

    update Repository module

--- DaggerCore/src/main/java/ir/omidtaheri/daggercore/di/components/ApplicationComponent.kt
+++ DaggerCore/src/main/java/ir/omidtaheri/daggercore/di/components/ApplicationComponent.kt
@@ -5,6 +5,7 @@ import dagger.Component
 import ir.omidtaheri.daggercore.di.modules.LocalDataSourceModule
 import ir.omidtaheri.daggercore.di.modules.RemoteDataSourceModule
 import ir.omidtaheri.domain.gateway.DiscoverMovieGateWay
+import ir.omidtaheri.domain.gateway.FavoriteMovieGateWay
 import ir.omidtaheri.domain.gateway.MovieGateWay
 import ir.omidtaheri.movotlin.di.modules.ApplicationModule
 import ir.omidtaheri.movotlin.di.modules.RepositoryModule
@@ -16,5 +17,6 @@ interface ApplicationComponent {
     fun schedulers(): ir.omidtaheri.domain.interactor.base.Schedulers
     fun MovieGateWayRepo(): MovieGateWay
     fun MovieDetailGateWayRepo():DiscoverMovieGateWay
+    fun FavoriteMovieGateWayRepo(): FavoriteMovieGateWay
     fun application():Application
 }
--- DaggerCore/src/main/java/ir/omidtaheri/daggercore/di/schedulers/AppScheduler.kt
+++ DaggerCore/src/main/java/ir/omidtaheri/daggercore/di/schedulers/AppScheduler.kt
@@ -1,13 +1,15 @@
 package ir.omidtaheri.daggercore.di.schedulers
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+
 import ir.omidtaheri.domain.interactor.base.Schedulers
 import javax.inject.Inject
 
+
 class AppScheduler @Inject constructor() : Schedulers {
     override val subscribeOn: Scheduler
-        get() = io.reactivex.rxjava3.schedulers.Schedulers.io()
+        get() = io.reactivex.schedulers.Schedulers.io()
     override val observeOn: Scheduler
         get() = AndroidSchedulers.mainThread()
 }
