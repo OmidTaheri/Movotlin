4ee9b0db2edf8401ea647ae888985139a8e02c52
OmidTaheri <m.omidtaheri@yahoo.com>
2020-08-24 17:42:50 +0430

    bug fixed in view model

--- Search/src/main/java/ir/omidtaheri/search/ui/SearchFragment/viewmodel/SearchViewModelFactory.kt
+++ Search/src/main/java/ir/omidtaheri/search/ui/SearchFragment/viewmodel/SearchViewModelFactory.kt
@@ -4,19 +4,20 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import ir.omidtaheri.domain.interactor.SearchMoviesByQuery
+import ir.omidtaheri.search.mapper.MovieEntityUiDomainMapper
 import ir.omidtaheri.search.mapper.MultiMovieEntityUiDomainMapper
 
 import javax.inject.Inject
 
 class SearchViewModelFactory @Inject constructor(
     val searchMoviesByQuery: SearchMoviesByQuery,
-    val multiMovieEntityUiDomainMapper: MultiMovieEntityUiDomainMapper,
+    val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
     val application: Application
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return SearchViewModel(
             searchMoviesByQuery,
-            multiMovieEntityUiDomainMapper,
+            movieEntityUiDomainMapper,
             application
         ) as T
     }
