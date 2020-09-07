2669827ca7aa38e4d46b67c76078a79cd61df4cc
OmidTaheri <m.omidtaheri@yahoo.com>
2020-09-08 00:09:41 +0430

    bug fixed in paging in serach movie interactor

--- Domain/src/main/java/ir/omidtaheri/domain/interactor/SearchMoviesByQuery.kt
+++ Domain/src/main/java/ir/omidtaheri/domain/interactor/SearchMoviesByQuery.kt
@@ -3,6 +3,7 @@ package ir.omidtaheri.domain.interactor
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.rxjava2.cachedIn
 import androidx.paging.rxjava2.observable
 import io.reactivex.Observable
 import ir.omidtaheri.domain.entity.MovieDomainEntity
@@ -10,6 +11,7 @@ import ir.omidtaheri.domain.gateway.DiscoverMovieGateWay
 import ir.omidtaheri.domain.interactor.base.ObservablePagingDataUseCase
 import ir.omidtaheri.domain.interactor.base.Schedulers
 import ir.omidtaheri.domain.paigingSource.SearchMovieByQuerySource
+import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 class SearchMoviesByQuery @Inject constructor(
@@ -21,7 +23,13 @@ class SearchMoviesByQuery @Inject constructor(
     override fun buildSingle(params: String): Observable<PagingData<MovieDomainEntity>> {
         return Pager(
             config = PagingConfig(PAGE_SIZE),
-            pagingSourceFactory = { SearchMovieByQuerySource(params, discoverMovieRepository,schedulers) }
+            pagingSourceFactory = {
+                SearchMovieByQuerySource(
+                    params,
+                    discoverMovieRepository,
+                    schedulers
+                )
+            }
         ).observable
     }
 
