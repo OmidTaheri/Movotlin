617c6cd356b4cc6d503ba974bd802d522b465b70
OmidTaheri <m.omidtaheri@yahoo.com>
2020-10-05 11:44:43 +0330

    no change

--- Domain/src/main/java/ir/omidtaheri/domain/interactor/SearchMoviesByQuery.kt
+++ Domain/src/main/java/ir/omidtaheri/domain/interactor/SearchMoviesByQuery.kt
@@ -3,7 +3,6 @@ package ir.omidtaheri.domain.interactor
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.rxjava2.cachedIn
 import androidx.paging.rxjava2.observable
 import io.reactivex.Observable
 import ir.omidtaheri.domain.entity.MovieDomainEntity
@@ -11,7 +10,6 @@ import ir.omidtaheri.domain.gateway.DiscoverMovieGateWay
 import ir.omidtaheri.domain.interactor.base.ObservablePagingDataUseCase
 import ir.omidtaheri.domain.interactor.base.Schedulers
 import ir.omidtaheri.domain.paigingSource.SearchMovieByQuerySource
-import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 class SearchMoviesByQuery @Inject constructor(
@@ -21,6 +19,7 @@ class SearchMoviesByQuery @Inject constructor(
     ObservablePagingDataUseCase<String, PagingData<MovieDomainEntity>>(schedulers) {
 
     override fun buildSingle(params: String): Observable<PagingData<MovieDomainEntity>> {
+
         return Pager(
             config = PagingConfig(PAGE_SIZE),
             pagingSourceFactory = {
