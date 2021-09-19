be9a33c553c51cde03b06c479e8278e936a69445
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-19 21:40:31 +0430

    Refactor GenreViewModelFactory base on lint warning

--- GenreList/src/main/java/ir/omidtaheri/genrelist/ui/GenreFragment/viewmodel/GenreViewModelFactory.kt
+++ GenreList/src/main/java/ir/omidtaheri/genrelist/ui/GenreFragment/viewmodel/GenreViewModelFactory.kt
@@ -2,17 +2,15 @@ package ir.omidtaheri.genrelist.ui.GenreFragment.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import ir.omidtaheri.androidbase.viewmodelutils.ViewModelAssistedFactory
 import ir.omidtaheri.domain.interactor.GetGenreList
 import ir.omidtaheri.genrelist.mapper.GenreEntityUiDomainMapper
 import javax.inject.Inject
 
 class GenreViewModelFactory @Inject constructor(
-    val getGenreList: GetGenreList,
-    val genreEntityUiDomainMapper: GenreEntityUiDomainMapper,
-    val application: Application
+    private val getGenreList: GetGenreList,
+    private val genreEntityUiDomainMapper: GenreEntityUiDomainMapper,
+    private val application: Application
 ) : ViewModelAssistedFactory<GenreViewModel> {
     override fun create(handle: SavedStateHandle): GenreViewModel {
         return GenreViewModel(
