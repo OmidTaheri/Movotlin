4f22d327462fb50f9bf7a5f4182d03ebaa6de6cd
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-19 20:40:37 +0430

    Update FavoriteViewModelFactory for handle (SavedStateHandle) by dagger

--- Favorite/src/main/java/ir/omidtaheri/favorite/ui/FavoriteFragment/viewmodel/FavoriteViewModelFactory.kt
+++ Favorite/src/main/java/ir/omidtaheri/favorite/ui/FavoriteFragment/viewmodel/FavoriteViewModelFactory.kt
@@ -1,23 +1,25 @@
 package ir.omidtaheri.favorite.ui.FavoriteFragment.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import ir.omidtaheri.domain.interactor.GetFavoriedMovieList
+import androidx.lifecycle.SavedStateHandle
+import ir.omidtaheri.androidbase.viewmodelutils.ViewModelAssistedFactory
 import ir.omidtaheri.domain.interactor.GetFavoriedMovieListByFlowable
 import ir.omidtaheri.favorite.mapper.FavoritedMovieEntityUiDomainMapper
 import javax.inject.Inject
 
 class FavoriteViewModelFactory @Inject constructor(
-    val getFavoriedMovieListByFlowable: GetFavoriedMovieListByFlowable,
-    val favoritedMovieEntityUiDomainMapper: FavoritedMovieEntityUiDomainMapper,
-    val application: Application
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    private val getFavoriedMovieListByFlowable: GetFavoriedMovieListByFlowable,
+    private val favoritedMovieEntityUiDomainMapper: FavoritedMovieEntityUiDomainMapper,
+    private val application: Application
+) : ViewModelAssistedFactory<FavoriteViewModel> {
+
+    override fun create(handle: SavedStateHandle): FavoriteViewModel {
         return FavoriteViewModel(
             getFavoriedMovieListByFlowable,
             favoritedMovieEntityUiDomainMapper,
+            handle,
             application
-        ) as T
+        )
     }
+
 }
