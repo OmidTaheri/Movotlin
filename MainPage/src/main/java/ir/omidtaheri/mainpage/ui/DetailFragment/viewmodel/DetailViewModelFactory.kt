78616c95391c0d06550531bbe264e39a4675d7bd
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-19 22:08:47 +0430

    Refactor DetailViewModelFactory for handle SavedStateHandle

--- MainPage/src/main/java/ir/omidtaheri/mainpage/ui/DetailFragment/viewmodel/DetailViewModelFactory.kt
+++ MainPage/src/main/java/ir/omidtaheri/mainpage/ui/DetailFragment/viewmodel/DetailViewModelFactory.kt
@@ -1,30 +1,31 @@
 package ir.omidtaheri.mainpage.ui.DetailFragment.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.SavedStateHandle
+import ir.omidtaheri.androidbase.viewmodelutils.ViewModelAssistedFactory
 import ir.omidtaheri.domain.interactor.*
 import ir.omidtaheri.domain.interactor.base.Schedulers
 import ir.omidtaheri.mainpage.mapper.*
 import javax.inject.Inject
 
 class DetailViewModelFactory @Inject constructor(
-    val getDetailMovieUseCase: GetMovieDetail,
-    val getMovieImagesById: GetMovieImagesById,
-    val getMovieVideosById: GetMovieVideosById,
-    val getSimilarMovies: GetSimilarMoviesSinglePage,
-    val favorieMovie: FavorieMovie,
-    val unfavoriteMovie: UnfavoriteMovie,
-    val getFavoriedMovieList: GetFavoriedMovieList,
-    val movieDetailEntityUiDomainMapper: MovieDetailEntityUiDomainMapper,
-    val movieImageEntityUiDomainMapper: MovieImageEntityUiDomainMapper,
-    val movieVideoEntityUiDomainMapper: MovieVideoEntityUiDomainMapper,
-    val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
-    val favoritedMovieEntityUiDomainMapper: FavoritedMovieEntityUiDomainMapper,
-    val schedulers: Schedulers,
-    val application: Application
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    private val getDetailMovieUseCase: GetMovieDetail,
+    private val getMovieImagesById: GetMovieImagesById,
+    private val getMovieVideosById: GetMovieVideosById,
+    private val getSimilarMovies: GetSimilarMoviesSinglePage,
+    private val favorieMovie: FavorieMovie,
+    private val unfavoriteMovie: UnfavoriteMovie,
+    private val getFavoriedMovieList: GetFavoriedMovieList,
+    private val movieDetailEntityUiDomainMapper: MovieDetailEntityUiDomainMapper,
+    private val movieImageEntityUiDomainMapper: MovieImageEntityUiDomainMapper,
+    private val movieVideoEntityUiDomainMapper: MovieVideoEntityUiDomainMapper,
+    private val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
+    private val favoritedMovieEntityUiDomainMapper: FavoritedMovieEntityUiDomainMapper,
+    private val schedulers: Schedulers,
+    private val application: Application
+) : ViewModelAssistedFactory<DetailViewModel> {
+
+    override fun create(handle: SavedStateHandle): DetailViewModel {
         return DetailViewModel(
             getDetailMovieUseCase,
             getMovieImagesById,
@@ -39,7 +40,8 @@ class DetailViewModelFactory @Inject constructor(
             movieEntityUiDomainMapper,
             favoritedMovieEntityUiDomainMapper,
             schedulers,
+            handle,
             application
-        ) as T
+        )
     }
 }
