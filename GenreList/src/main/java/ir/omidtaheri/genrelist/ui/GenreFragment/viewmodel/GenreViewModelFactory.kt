4900b258144e27007991cacc7ef468c60beb74fe
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-19 21:26:33 +0430

    Refactor GenreViewModelFactory for handle SavedStateHandle

--- GenreList/src/main/java/ir/omidtaheri/genrelist/ui/GenreFragment/viewmodel/GenreViewModelFactory.kt
+++ GenreList/src/main/java/ir/omidtaheri/genrelist/ui/GenreFragment/viewmodel/GenreViewModelFactory.kt
@@ -1,8 +1,10 @@
 package ir.omidtaheri.genrelist.ui.GenreFragment.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import ir.omidtaheri.androidbase.viewmodelutils.ViewModelAssistedFactory
 import ir.omidtaheri.domain.interactor.GetGenreList
 import ir.omidtaheri.genrelist.mapper.GenreEntityUiDomainMapper
 import javax.inject.Inject
@@ -11,12 +13,14 @@ class GenreViewModelFactory @Inject constructor(
     val getGenreList: GetGenreList,
     val genreEntityUiDomainMapper: GenreEntityUiDomainMapper,
     val application: Application
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+) : ViewModelAssistedFactory<GenreViewModel> {
+    override fun create(handle: SavedStateHandle): GenreViewModel {
         return GenreViewModel(
             getGenreList,
             genreEntityUiDomainMapper,
+            handle,
             application
-        ) as T
+        )
     }
+
 }
