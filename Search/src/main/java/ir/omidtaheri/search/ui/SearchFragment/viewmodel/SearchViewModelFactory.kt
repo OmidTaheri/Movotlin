eff80158da84ac18618a77869d5a79a9ce5adeb7
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-20 00:06:45 +0430

    Refactor SearchViewModelFactory for handle SavedStateHandle

--- Search/src/main/java/ir/omidtaheri/search/ui/SearchFragment/viewmodel/SearchViewModelFactory.kt
+++ Search/src/main/java/ir/omidtaheri/search/ui/SearchFragment/viewmodel/SearchViewModelFactory.kt
@@ -1,25 +1,26 @@
 package ir.omidtaheri.search.ui.SearchFragment.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.SavedStateHandle
+import ir.omidtaheri.androidbase.viewmodelutils.ViewModelAssistedFactory
 import ir.omidtaheri.domain.interactor.SearchMoviesByQuery
 import ir.omidtaheri.domain.interactor.base.Schedulers
 import ir.omidtaheri.search.mapper.MovieEntityUiDomainMapper
 import javax.inject.Inject
 
 class SearchViewModelFactory @Inject constructor(
-    val searchMoviesByQuery: SearchMoviesByQuery,
-    val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
-    val schedulers: Schedulers,
-    val application: Application
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    private val searchMoviesByQuery: SearchMoviesByQuery,
+    private val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
+    private val schedulers: Schedulers,
+    private val application: Application
+) : ViewModelAssistedFactory<SearchViewModel> {
+    override fun create(handle: SavedStateHandle): SearchViewModel {
         return SearchViewModel(
             searchMoviesByQuery,
             movieEntityUiDomainMapper,
             schedulers,
+            handle,
             application
-        ) as T
+        )
     }
 }
