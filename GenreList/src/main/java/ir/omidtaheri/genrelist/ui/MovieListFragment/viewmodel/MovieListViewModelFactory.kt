84a8f0a6fe96c390c5083e9657af37abd75a7d47
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-19 21:42:59 +0430

    Refactor MovieListViewModelFactory for handle SavedStateHandle

--- GenreList/src/main/java/ir/omidtaheri/genrelist/ui/MovieListFragment/viewmodel/MovieListViewModelFactory.kt
+++ GenreList/src/main/java/ir/omidtaheri/genrelist/ui/MovieListFragment/viewmodel/MovieListViewModelFactory.kt
@@ -1,22 +1,24 @@
 package ir.omidtaheri.genrelist.ui.MovieListFragment.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.SavedStateHandle
+import ir.omidtaheri.androidbase.viewmodelutils.ViewModelAssistedFactory
 import ir.omidtaheri.domain.interactor.GetMovieListByGenreId
 import ir.omidtaheri.genrelist.mapper.MovieEntityUiDomainMapper
 import javax.inject.Inject
 
 class MovieListViewModelFactory @Inject constructor(
-    val getMovieListByGenreId: GetMovieListByGenreId,
-    val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
-    val application: Application
-) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    private val getMovieListByGenreId: GetMovieListByGenreId,
+    private val movieEntityUiDomainMapper: MovieEntityUiDomainMapper,
+    private val application: Application
+) : ViewModelAssistedFactory<MovieListViewModel> {
+
+    override fun create(handle: SavedStateHandle): MovieListViewModel {
         return MovieListViewModel(
             getMovieListByGenreId,
             movieEntityUiDomainMapper,
+            handle,
             application
-        ) as T
+        )
     }
 }
