8a37127fb0a70ac71a24b0d3720aa5465d80c6bf
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-20 17:11:14 +0430

    Refactor application component in TvApplicationClass

--- tvapp/src/main/java/ir/omidtaheri/tvapp/TvApplicationClass.kt
+++ tvapp/src/main/java/ir/omidtaheri/tvapp/TvApplicationClass.kt
@@ -4,25 +4,18 @@ import androidx.multidex.MultiDexApplication
 import ir.omidtaheri.daggercore.di.ApplicationComponentProvider
 import ir.omidtaheri.movotlin.di.components.ApplicationComponent
 import ir.omidtaheri.movotlin.di.components.DaggerApplicationComponent
-import ir.omidtaheri.movotlin.di.modules.ApplicationModule
-import ir.omidtaheri.movotlin.di.modules.LocalModule
-import ir.omidtaheri.movotlin.di.modules.RemoteModule
-import ir.omidtaheri.movotlin.di.modules.RepositoryModule
 
 class TvApplicationClass : MultiDexApplication(), ApplicationComponentProvider {
 
-    lateinit var applicationComponent: ApplicationComponent
+    private lateinit var applicationComponent: ApplicationComponent
 
     override fun onCreate() {
         super.onCreate()
 
-        this.applicationComponent =
-            DaggerApplicationComponent.builder()
-                .applicationModule(ApplicationModule(this))
-                .localModule(LocalModule("Movotlin"))
-                .remoteModule(RemoteModule(BuildConfig.BASE_URL, BuildConfig.API_KEY))
-                .repositoryModule(RepositoryModule())
-                .build()
+        applicationComponent =
+            DaggerApplicationComponent.factory()
+                .create(this, "Movotlin", BuildConfig.BASE_URL, BuildConfig.API_KEY)
+
 
         applicationComponent.inject(this)
     }
