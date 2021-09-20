097fb7895ed98be849becac0e9ae186d511c09b1
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-20 22:16:02 +0430

    Refactor GalleryViewAdapter for load poster only

--- MainPage/src/main/java/ir/omidtaheri/mainpage/ui/MainFragment/adapters/GalleryViewAdapter.kt
+++ MainPage/src/main/java/ir/omidtaheri/mainpage/ui/MainFragment/adapters/GalleryViewAdapter.kt
@@ -9,21 +9,15 @@ import ir.omidtaheri.androidbase.BaseViewHolder
 import ir.omidtaheri.mainpage.databinding.MovieViewerEmptyStateBinding
 import ir.omidtaheri.mainpage.databinding.MovieViewerItemBinding
 import ir.omidtaheri.mainpage.entity.MovieUiEntity
-import ir.omidtaheri.uibase.LoadBackdrop
 import ir.omidtaheri.uibase.LoadPoster
 import ir.omidtaheri.uibase.clear
 
 
-class GalleryViewAdapter(diffCallback: DiffUtil.ItemCallback<MovieUiEntity>,val context: Context) :
+class GalleryViewAdapter(diffCallback: DiffUtil.ItemCallback<MovieUiEntity>, val context: Context) :
     PagingDataAdapter<MovieUiEntity, BaseViewHolder>(diffCallback) {
 
-//
-//    var items: MutableList<MovieUiEntity> = mutableListOf()
-//
-//    val VIEW_TYPE_EMPTY = 0
-//    val VIEW_TYPE_NORMAL = 1
 
-    lateinit var mCallback: Callback
+    private lateinit var mCallback: Callback
 
     interface Callback {
         fun onItemClick(movieId: Int)
@@ -34,20 +28,7 @@ class GalleryViewAdapter(diffCallback: DiffUtil.ItemCallback<MovieUiEntity>,val 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-//
-//        when (viewType) {
-//
-//            VIEW_TYPE_EMPTY -> {
-//                return EmptyViewHolder(
-//                    MovieViewerEmptyStateBinding.inflate(
-//                        LayoutInflater.from(parent.context),
-//                        parent,
-//                        false
-//                    )
-//                )
-//            }
-//
-//            else ->
+
         return ViewHolder(
             MovieViewerItemBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -55,57 +36,13 @@ class GalleryViewAdapter(diffCallback: DiffUtil.ItemCallback<MovieUiEntity>,val 
                 false
             )
         )
-//
-//        }
+
     }
-//
-//    override fun getItemCount(): Int {
-//        return if (items != null && items.size != 0) {
-//            items.size
-//        } else {
-//            1
-//        }
-//    }
-//
-//    override fun getItemViewType(position: Int): Int {
-//        return if (items != null && items.size > 0) {
-//            VIEW_TYPE_NORMAL
-//        } else {
-//            VIEW_TYPE_EMPTY
-//        }
-//
-//    }
-//
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         holder.onBind(position)
     }
 
-//
-//    //    Helpers
-//    fun addItem(item: MovieUiEntity) {
-//        items.add(item)
-//        notifyItemInserted(items.size - 1)
-//    }
-//
-//    fun addItems(list: List<MovieUiEntity>) {
-//        items.addAll(list)
-//        notifyDataSetChanged()
-//    }
-//
-//    fun remove(item: MovieUiEntity) {
-//        val index = items.indexOf(item)
-//        if (index >= 0) {
-//            items.removeAt(index)
-//            notifyItemRemoved(index)
-//        }
-//    }
-//
-//    fun clear() {
-//        items.clear()
-//        notifyDataSetChanged()
-//    }
-//
 
     inner class ViewHolder(val binding: MovieViewerItemBinding) : BaseViewHolder(binding.root) {
 
@@ -113,12 +50,13 @@ class GalleryViewAdapter(diffCallback: DiffUtil.ItemCallback<MovieUiEntity>,val 
             val movieUiEntity = getItem(position)
 
             binding.apply {
-                movieUiEntity?.posterPath?.let { movieImageView.LoadPoster(it,context) }
-                    ?: movieUiEntity?.backdropPath?.let { movieImageView.LoadBackdrop(it,context) }
+
+                movieUiEntity?.posterPath?.let { movieImageView.LoadPoster(it, context) }
                 titleMovie.text = movieUiEntity!!.title
                 root.setOnClickListener {
                     mCallback.onItemClick(movieUiEntity.id)
                 }
+
             }
         }
     }
@@ -145,7 +83,6 @@ class GalleryViewAdapter(diffCallback: DiffUtil.ItemCallback<MovieUiEntity>,val 
 
 object MovieUiEntityComparator : DiffUtil.ItemCallback<MovieUiEntity>() {
     override fun areItemsTheSame(oldItem: MovieUiEntity, newItem: MovieUiEntity): Boolean {
-        // Id is unique.
         return oldItem.id == newItem.id
     }
 
