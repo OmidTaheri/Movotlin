49bbae3774eece73bbbfdf01e38336312b583a40
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-09-22 23:43:58 +0330

    Update version of recyclerview to 1.2.1

--- buildSrc/src/main/java/BuildDependenciesVersions.kt
+++ buildSrc/src/main/java/BuildDependenciesVersions.kt
@@ -38,7 +38,7 @@ object BuildDependenciesVersions {
     object UiDependenciesVersions {
         const val APPCOMPAT = "1.1.0"
         const val MATERIAL = "1.3.0-alpha01"
-        const val RECYCLE_VIEW = "1.2.0-alpha03"
+        const val RECYCLE_VIEW = "1.2.1"
         const val CONSTRAINT_LAYOUT = "2.0.0-beta7"
         const val CARDVIEW = "1.0.0"
         const val VIEWPAGER2 = "1.0.0"
