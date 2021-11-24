c61eb55186492837b02c87a41e86f7841910702d
M.OmidTaheri <m.omidtaheri@gmail.com>
2021-11-24 18:48:39 +0330

    Update compileSDK to 31

--- buildSrc/src/main/java/BuildAndroidConfig.kt
+++ buildSrc/src/main/java/BuildAndroidConfig.kt
@@ -3,8 +3,8 @@ object BuildAndroidConfig {
     const val APPLICATION_ID = "ir.omidtaheri.movotlin"
     const val TV_APPLICATION_ID = "ir.omidtaheri.movotlintv"
 
-    const val BUILD_TOOLS_VERSION = "30.0.2"
-    const val COMPILE_SDK_VERSION =30
+    const val BUILD_TOOLS_VERSION = "31.0.0"
+    const val COMPILE_SDK_VERSION =31
     const val MIN_SDK_VERSION =21
     const val TV_MIN_SDK_VERSION =21
     const val TARGET_SDK_VERSION =30
